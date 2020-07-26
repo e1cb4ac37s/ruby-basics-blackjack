e4aef0d8b923c960c97c1d9f25e1dee79e169842
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-07-26 21:03:35 +0300

    returned 10

--- game/modules/constants.rb
+++ game/modules/constants.rb
@@ -3,7 +3,7 @@
 module Constants
   ACE = 'A'
   JQK = %w[J Q K].freeze
-  INDEX_RANKS = %w[2 3 4 5 6 7 8 9].freeze
+  INDEX_RANKS = %w[2 3 4 5 6 7 8 9 10].freeze
   RANKS = [ACE, JQK, INDEX_RANKS].flatten
   SUITS = %w[♦ ♣ ♥ ♠].freeze
 end
